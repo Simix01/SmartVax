052e78af80e2e8334b45c101f0c6a9dc4c62c93c
Joshua Randez Perez <joshuarandez06@gmail.com>
2022-07-27 22:48:08 +0200

    Fourth Commit

--- PROVA4/src/main/java/PROVA4.java
+++ PROVA4/src/main/java/PROVA4.java
@@ -1,6 +1,14 @@
+import java.beans.Statement;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.Arrays;
+import java.sql.Connection;
+
 
 public class PROVA4 {
+	
+
+
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -10,5 +18,26 @@ public class PROVA4 {
 	      Arrays.asList(args).forEach(System.out::println);
 	    else
 	      System.out.println("No arguments recieved!");
+	    
+	    ConnectDB c = new ConnectDB(args[0],args[1]);
+	    
+	    Connection conn=c.connect();
+	    
+	    String query = "insert into persons values (4,'ciao','ciao','ciao','ciao')";
+
+	    try {
+	    	
+			PreparedStatement preparedStmt = conn.prepareStatement(query);
+			preparedStmt.execute();
+			conn.close();
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	    
+	    
+	    
 	}
+	
 }
