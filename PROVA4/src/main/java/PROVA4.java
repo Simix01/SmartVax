fe4251a15a6cf7816434bfc6654fa2eaef8004a5
Joshua Randez Perez <joshuarandez06@gmail.com>
2022-08-01 16:38:02 +0200

    Sixth Commit

--- PROVA4/src/main/java/PROVA4.java
+++ PROVA4/src/main/java/PROVA4.java
@@ -1,43 +1,54 @@
-import java.beans.Statement;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Arrays;
+
+import common.CentroVaccinaleGiaRegistrato;
+import common.CentroVaccinaleServiceImpl;
+
 import java.sql.Connection;
 
-
 public class PROVA4 {
-	
 
-
-
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException, CentroVaccinaleGiaRegistrato, SQLException {
 		// TODO Auto-generated method stub
 
-	    System.out.println("---- Main Class1 -----");
-	    if(args.length > 0)
-	      Arrays.asList(args).forEach(System.out::println);
-	    else
-	      System.out.println("No arguments recieved!");
-	    
-	    ConnectDB c = new ConnectDB(args[0],args[1]);
-	    
-	    Connection conn=c.connect();
-	    
-	    String query = "insert into persons values (4,'ciao','ciao','ciao','ciao')";
+		System.out.println("---- Main Class1 -----");
+		if (args.length > 0)
+			Arrays.asList(args).forEach(System.out::println);
+		else
+			System.out.println("No arguments recieved!");
 
-	    try {
-	    	
-			PreparedStatement preparedStmt = conn.prepareStatement(query);
-			preparedStmt.execute();
-			conn.close();
-			
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		ConnectDB c = new ConnectDB("postgres", "Nhuari062799!");
+
+		Connection conn = c.connect();
+
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		ResultSet rs;
+
+
+		String queryControllo = "select nomecentro from centrivaccinali where nomecentro = 'Cmo'";
+		Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
+
+		rs = stmt.executeQuery(queryControllo);
+
+		if (!rs.isBeforeFirst())
+		{
+			System.out.print("No data");
 		}
-	    
-	    
-	    
+		else {
+			System.out.print("Si data");
+		}
+
+		//CentroVaccinaleServiceImpl prova=new CentroVaccinaleServiceImpl(conn);
+		//prova.registraCentroVaccinale();
+		
+		
+
 	}
-	
+
 }
